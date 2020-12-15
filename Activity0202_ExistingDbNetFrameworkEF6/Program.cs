6391dc0312f649afe9f4c4f002bf2eef94f375b8
ComputacionAplicada <75705119+ComputacionAplicada@users.noreply.github.com>
2020-12-15 16:18:08 -0700

    Agregar archivos de proyecto.

---
+++ Activity0202_ExistingDbNetFrameworkEF6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EF_Activity01_NetFramework;
+
+namespace Activity0202_ExistingDbNetFrameworkEF6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new AdventureWorksEntities())
+            { 
+                var people = db.People.OrderByDescending(x => x.LastName).Take(20);
+                foreach(var person in people)
+                { 
+                    foreach(var p in people)
+                    {
+                        Console.WriteLine($"{p.FirstName} - {p.LastName}");
+                    }
+                }
+            }
+        }
+    }
+}
